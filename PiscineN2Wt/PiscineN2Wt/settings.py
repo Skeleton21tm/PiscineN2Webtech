857bc36548ba53c6c81ef231bb6debea8d6000e5
Ogsamee23 <claude-emmanuel.samecken@next-u.fr>
2023-11-30 15:28:33 +0100

    ADD blog home contacts

--- PiscineN2Wt/PiscineN2Wt/settings.py
+++ PiscineN2Wt/PiscineN2Wt/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'PiscineN2Wt',
+    'blog',
+    'home',
+    'contacts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
