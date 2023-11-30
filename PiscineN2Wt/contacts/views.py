d406fc00fbe80399402de80bfcfeb008882b04d6
valentin-vegee <117643191+valentin-vegee@users.noreply.github.com>
2023-12-01 00:43:41 +0100

    ADD contact

--- PiscineN2Wt/contacts/views.py
+++ PiscineN2Wt/contacts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            ContactForm.objects.create(**form.cleaned_data)
+
+            # Process contact form data here
+
+            return redirect('liste_contact')
+        else:
+            form = ContactForm()
+    else:
+        form = ContactForm()
+
+    return render(request, 'contacts/contacts.html', {'form':form})
