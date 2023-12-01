c2730c281890f6d0ecc3fec0ddaaf37d89785dab
valentin-vegee <117643191+valentin-vegee@users.noreply.github.com>
2023-12-01 01:43:05 +0100

    CHANGE urls.py

--- PiscineN2Wt/PiscineN2Wt/urls.py
+++ PiscineN2Wt/PiscineN2Wt/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home.views import recherche_articles
+from django.urls import path, include
+from home.views import home 
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', recherche_articles, name='recherche_articles'),
+    path('', home, name='home'),
+    path('contact', include('contacts.urls'))
 ]
